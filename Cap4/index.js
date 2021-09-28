42ee40c80ea65624f207a218d83b302ac6fafe9c
Andre Alves Araujo <andrealvesaraujo.1995@gmail.com>
2021-09-28 17:58:53 -0300

    Update itens of Stack for object instead of array

--- Cap4/index.js
+++ Cap4/index.js
@@ -2,31 +2,58 @@ console.log("Stack Pilha")
 
 class Stack {
     constructor() {
-        this.items = []
+        this.count = 0
+        this.items = {}
     }
     
     push(element) {
-        this.items.push(element)
+        this.items[this.count] = element
+        this.count++
     }
     
     pop() {
-        return this.items.pop()
+ 
+        if(this.isEmpty()) {
+            return undefined
+        }
+
+        this.count--
+        const result = this.items[this.count]
+        delete this.items[this.count]
+        return result
     }
 
     peek() {
-        return this.items[this.items.length - 1]
+        if(this.isEmpty()) {
+            return undefined
+        }
+
+        return this.items[this.count - 1]
     }
 
     isEmpty() {
-        return this.items.length === 0
+        return this.count === 0
     }
 
     size() {
-        return this.items.length
+        return this.count
     }
 
     clear() {
-        this.items = []
+        this.items = {}
+        this.count = 0
+    }
+
+    toString() {
+        if(this.isEmpty()) {
+            return ""
+        }
+        let objString = `${this.items[0]}`
+        for (let i = 1; i < this.count; i++) {
+            objString = `${objString}, ${this.items[i]}`
+            
+        }
+        return objString
     }
 
 }
@@ -36,6 +63,8 @@ console.log(stack.isEmpty())
 
 stack.push(5)
 stack.push(8)
+
+console.log(stack)
 
 console.log(stack.peek())
 
@@ -47,4 +76,7 @@ stack.push(15)
 stack.pop()
 stack.pop()
 console.log(stack.size())
+
+console.log(stack.toString())
+
 
