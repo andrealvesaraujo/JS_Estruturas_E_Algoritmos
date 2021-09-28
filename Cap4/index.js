39eee92094fe9a7982341e7026bbae1dc1f7e44a
Andre Alves Araujo <andrealvesaraujo.1995@gmail.com>
2021-09-28 17:26:41 -0300

    Created Stack Estructure

--- Cap4/index.js
+++ Cap4/index.js
@@ -1,1 +1,32 @@
-console.log("Hello World")
+console.log("Stack Pilha")
+
+class Stack {
+    constructor() {
+        this.items = []
+    }
+    
+    push(element) {
+        this.items.push(element)
+    }
+    
+    pop() {
+        return this.items.pop()
+    }
+
+    peek() {
+        return this.items[this.items.length - 1]
+    }
+
+    isEmpty() {
+        return this.items.length === 0
+    }
+
+    size() {
+        return this.items.length
+    }
+
+    clear() {
+        this.items = []
+    }
+
+}
