ed977b7d2a0333aacf5080726dcb7d83a6c2895a
Andre Alves <andrealvesaraujo.1995@gmail.com>
2021-10-16 17:05:31 -0300

    Finished LinkedList

--- Cap6/index.js
+++ Cap6/index.js
@@ -27,9 +27,28 @@ export default class LinkedList {
         this.count++
     }
 
+    insert(element, index) {
+        if(index >= 0 && index <= this.size()) {
+            const node = new Node(element)
+            if(index===0){
+                let current = this.head
+                node.next = current
+                this.head = node
+            }else {
+                const previus = this.getElementAt(index-1)
+                let current = previus.next 
+                node.next = current
+                previus.next = node
+            }
+            this.count++
+            return true
+        }
+        return false 
+    }
+
     removeAt(index) {
 
-        if(index>=0 && index < this.count) {
+        if(index>=0 && index < this.size()) {
 
             let current = this.head
             if(index === 0){
@@ -48,7 +67,7 @@ export default class LinkedList {
 
     getElementAt(index) {
 
-        if(index>=0 && index < this.count) {
+        if(index>=0 && index < this.size()) {
 
             let node = this.head
 
@@ -61,6 +80,47 @@ export default class LinkedList {
         return undefined
     }
 
+    indexOf(element) {
+        let current = this.head 
+        for(let i = 0; i < this.size() && current!=null; i++){
+            if(this.equalsFn(element, current.element)){
+                return i
+            }
+            current = current.next
+        }
+        return -1 
+    }
+
+    remove(element) {
+        const index = this.indexOf(element)
+        return this.removeAt(index)
+    }
+
+    size() {
+        return this.count
+    }
+
+    isEmpty(){
+        return this.size() === 0
+    }
+
+    getHead() {
+        return this.head
+    }
+    
+    toString() {
+        if(this.isEmpty()){
+            return ''
+        }
+        let objString = `${this.head.element}`
+        let current = this.head.next
+        for (let i = 1; i < this.size() && current!=null; i++) {
+            objString = `${objString}, ${current.element}`
+            current = current.next            
+        }
+        return objString
+    }
+
 }
 
 const list = new LinkedList()
@@ -71,8 +131,29 @@ list.push(18)
 list.push(20)
 list.push(25)
 
-console.log(list)
-console.log("Removed Element: ", list.removeAt(3))
-console.log("Removed Element: ", list.removeAt(4))
-console.log(list)
+console.log(`List ${list}`)
+console.log("Removed Element of index 3: ", list.removeAt(3))
+console.log("Removed Element of index 4: ", list.removeAt(4))
+console.log(`List ${list}`)
+
+
 console.log("Search element at position 2: ", list.getElementAt(1).element)
+
+list.insert("A", 3)
+list.insert("B", 5)
+list.insert("C", 0)
+console.log(`List ${list}`)
+
+
+console.log("Search index of element 'A': ", list.indexOf("A"))
+console.log("Search index of element 'C': ", list.indexOf("C"))
+console.log("Search index of element 'ABC': ", list.indexOf("ABC"))
+console.log("Removed Element ''B': ", list.remove("B"))
+console.log(`List ${list}`)
+
+console.log("Head of List: ", list.getHead())
+console.log("List is Empty? ", list.isEmpty())
+console.log("List Size: ", list.size())
+
+
+
