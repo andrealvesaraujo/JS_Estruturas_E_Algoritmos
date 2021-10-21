25931f43c5fa21034ceccbb2fdc92a5fa7c48750
Andre Alves <andrealvesaraujo.1995@gmail.com>
2021-10-21 18:57:45 -0300

    Add CircularLinkedList

--- Cap6/index.js
+++ Cap6/index.js
@@ -67,7 +67,7 @@ export default class LinkedList {
 
     getElementAt(index) {
 
-        if(index>=0 && index < this.size()) {
+        if(index>=0 && index <= this.count) {
 
             let node = this.head
 
@@ -239,3 +239,75 @@ console.log("Removed Element of index 1: ", dblist.removeAt(1))
 console.log(`DoublyLinkedList ${dblist}`)
 
 
+class CircularLinkedList extends LinkedList {
+    constructor(equalsFn = defaultEquals) {
+        super(equalsFn)
+    }
+
+    insert(element, index) {
+        if(index >= 0 && index <= this.count){
+            const node = new Node(element)
+            let current = this.head
+            
+            if(index === 0){
+
+                if(this.head == null){
+                    this.head = node
+                    node.next = this.head
+                }else{
+                    node.next = current
+                    current = this.getElementAt(this.size())
+                    this.head = node
+                    current.next = this.head
+                }
+
+            }else{
+                const previus = this.getElementAt(index-1)
+                node.next = previus.next
+                previus.next = node
+            }
+            this.count++
+            return true
+        }
+        return false
+    }
+
+    removeAt(index) {
+        if(index >=0 && index < this.count) {
+            let current = this.head
+            if(index === 0){
+                if(this.size() === 1){
+                    this.head = undefined
+                }else{
+                    const removed = this.head
+                    current = this.getElementAt(this.size())
+                    this.head = this.head.next
+                    current.next = this.head
+                    current = removed
+                }
+            }else {
+                const previus = this.getElementAt(index-1)
+                current = previus.next
+                previus.next = current.next
+            }
+            this.count--
+            return current.element
+        }
+        return undefined
+    }
+
+}
+
+
+const circularlist = new CircularLinkedList()
+
+circularlist.insert("AB", 0)
+circularlist.insert("BC", 1)
+circularlist.insert("CD", 2)
+circularlist.insert("DE", 3)
+console.log(`CircularLinkedList ${circularlist}`)
+
+console.log("Removed Element of index 0: ", circularlist.removeAt(0))
+console.log("Removed Element of index 2: ", circularlist.removeAt(2))
+console.log("Removed Element of index 1: ", circularlist.removeAt(1))
+console.log(`CircularLinkedList ${circularlist}`)
