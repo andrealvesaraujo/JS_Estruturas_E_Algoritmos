91015a8ec5027a6514f0b3b524f242e14695e725
Andre Alves <andrealvesaraujo.1995@gmail.com>
2021-10-21 16:57:03 -0300

    Add DoublyLinkedList

--- Cap6/index.js
+++ Cap6/index.js
@@ -1,5 +1,5 @@
 import {defaultEquals} from "/utils.js"
-import {Node} from "./models/Node.js"
+import {Node, DoublyNode} from "./models/Node.js"
 
 console.log("Linked List")
 
@@ -155,5 +155,87 @@ console.log("Head of List: ", list.getHead())
 console.log("List is Empty? ", list.isEmpty())
 console.log("List Size: ", list.size())
 
+class DoublyLinkedList extends LinkedList{
+    constructor(equalsFn = defaultEquals) {
+        super(equalsFn)
+        this.tail = undefined
+    }
+
+    insert(element, index) {
+        if(index>=0 && index<=this.count){
+            const node = new DoublyNode(element)
+            let current = this.head
+            if(index === 0){
+
+                if(this.head == null){
+                    this.head = node;
+                    this.tail = node;
+                } else {
+                    node.next = this.head
+                    current.prev = node
+                    this.head = node
+                }
+
+            }else if(index === this.count) {
+                current = this.tail
+                current.next = node
+                node.prev = current
+                this.tail = node
+            } else {
+                const previus = this.getElementAt(index-1)
+                current = previus.next
+                node.next = current
+                previus.next = node
+                current.prev = node 
+                node.prev = previus
+            }
+            this.count++
+            return true
+        }
+        return false
+    }
+
+    removeAt(index) {
+        if(index >=0 && index < this.count){
+            let current = this.head
+            if(index === 0){
+                this.head = current.next
+                if(this.count === 1){
+                    this.tail = undefined
+                }else{
+                    this.head.prev = undefined
+                }
+            }else if(index === this.count -1){
+                current = this.tail
+                this.tail = current.prev
+                this.tail.next = undefined
+            }else{
+                current = this.getElementAt(index)
+                const previous = current.prev
+                console.log(previus)
+                previous.next = current.next
+                current.next.prev = previous
+            }
+            this.count--
+            return current.element
+        }
+        return undefined
+    }
+
+}
+
+
+const dblist = new DoublyLinkedList()
+
+dblist.insert("A", 0)
+dblist.insert("B", 1)
+dblist.insert("C", 2)
+dblist.insert("D", 3)
+console.log(`DoublyLinkedList ${dblist}`)
+
+console.log("Removed Element of index 0: ", dblist.removeAt(0))
+console.log("Removed Element of index 2: ", dblist.removeAt(2))
+console.log("Removed Element of index 1: ", dblist.removeAt(1))
+console.log(`DoublyLinkedList ${dblist}`)
 
 
