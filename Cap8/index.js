32714111284b0986a5b2325f4e6a27bd405bf210
Andre Alves <andrealvesaraujo.1995@gmail.com>
2021-11-18 11:05:48 -0300

    Create Dictionary Class and Utils Function

--- Cap8/index.js
+++ Cap8/index.js
@@ -1,1 +1,10 @@
-console.log("Hello world!");
+import {defaultToString} from "/utils.js"
+
+export default class Dictionary {
+    constructor(toStrFn = defaultToString) {
+        this.toStrFn = toStrFn;
+        this.table = {}
+    }
+}
+
+console.log("My Dictionary");
