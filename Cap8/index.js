9579b45e2f92f21e272aa62a9f4fba05d5cc4955
Andre Alves <andrealvesaraujo.1995@gmail.com>
2021-11-18 11:48:36 -0300

    Add all methods of dictionary

--- Cap8/index.js
+++ Cap8/index.js
@@ -1,9 +1,118 @@
 import {defaultToString} from "/utils.js"
 
+class ValuePair {
+    constructor(key,value) { 
+        this.key = key;
+        this.value = value;
+    }
+    toString() {
+        return `#${this.key}: ${this.value}`;
+    }
+}
+
 export default class Dictionary {
     constructor(toStrFn = defaultToString) {
         this.toStrFn = toStrFn;
         this.table = {}
+    }
+
+    hasKey(key) {
+        return this.table[this.toStrFn(key)] != null
+    }
+
+    set(key, value) {
+        if(key != null && value != null) {
+            const tableKey = this.toStrFn(key)
+            this.table[tableKey] = new ValuePair(key, value)
+            return true
+        }
+        return false;
+    }
+
+    remove(key) {
+        if(this.hasKey(key)) {
+            delete this.table[this.toStrFn(key)]
+            return true
+        }
+        return false
+    }
+
+    get(key) {
+        const valuePair = this.table[this.toStrFn(key)]
+        return valuePair == null ? undefined : valuePair.value
+
+        // OR
+
+        // if(this.hasKey(key)) {
+        //     return this.table[this.toStrFn(key)]
+        // }
+        // return undefined
+    }
+
+    keyValues() {
+        return Object.values(this.table)
+
+        // OR
+
+        // const valuePair = []
+        // for (const k in this.table) {
+        //     if(this.hasKey(k)) {
+        //         valuePair.push(this.table[k])
+        //     }
+        // }
+        // return valuePair
+    }
+
+    keys(){
+        return this.keyValues().map(valuePair => valuePair.key)
+
+        // OR
+
+        // const keys = []
+        // const valuePair = this.keyValues()
+        // for(let i = 0; i < valuePair.length; i++) {
+        //     keys.push(valuePair[i].key)
+        // }
+        // return keys
+
+    }
+
+    values(){
+        return this.keyValues().map(valuePair => valuePair.value)
+    }
+
+    forEach(callbackFn){
+        const valuePairs = this.keyValues()
+        for(let i = 0; i < valuePairs.length; i++) {
+            const result = callbackFn(valuePairs[i].key, valuePairs[i].value)
+            if(result === false) {
+                break
+            }
+        }
+    }
+
+    size() {
+        return Object.key(this.keyValues()).length;
+    }
+
+    isEmpty() {
+        return this.size() === 0
+    }
+
+    clear() {
+        this.table = {}
+    }
+
+    toString() {
+        if(this.isEmpty()){
+            return ''
+        }
+        const valuePairs = this.keyValues()
+        let objString = `${valuePairs[0].toString()}`
+        for(let i = 1; i< valuePairs.length; i++) {
+            objString = `${objString}, ${valuePairs[i].toString()}`
+        }
+        return objString
     }
 }
 
