d0329eb05885fa4b5bba609a58239f367b987242
Andre Alves <andrealvesaraujo.1995@gmail.com>
2021-11-19 12:33:56 -0300

    Create HashTable Class and his methods

--- Cap8/index.js
+++ Cap8/index.js
@@ -143,7 +143,116 @@ console.log("Dictionary keys: ",dictionary.keys())
 console.log("Dictionary values: ", dictionary.values())
 console.log("Dictionary keyValues: ", dictionary.keyValues())
 
+console.log(dictionary.toString())
+
 console.log('Dictionary forEach: ')
 dictionary.forEach((k,v)=>{
     console.log(`key: ${k}, value: ${v}`)
 })
+
+console.log("--------------------------------")
+
+console.log("My HashTable");
+
+class HashTable {
+    constructor(toStrFn = defaultToString) {
+        this.toStrFn = toStrFn
+        this.table = {}
+    }
+
+    loseloseHashCode(key) {
+        if(typeof key === 'number') {       
+            return key
+        }
+        const tableKey = this.toStrFn(key)
+        let hash = 0
+        for(let i = 0; i < tableKey.length; i++) {
+            hash += tableKey.charCodeAt(i)
+        }
+        return hash % 37
+    }
+
+    hashCode(key) {
+        return this.loseloseHashCode(key)
+    }
+
+    put(key, value) {
+        if(key !== null && value !== null) {
+            const position = this.hashCode(key)
+            this.table[position] = new ValuePair(key,value)
+            return true
+        }
+        return false
+    }
+
+    get(key){
+        const valuePair = this.table[this.hashCode(key)]
+        return valuePair == null ? null : valuePair.value
+    }
+
+    remove(key) {
+        const hash = this.hashCode(key)
+        const valuePair = this.table[hash]
+        if(valuePair != null) {
+            delete this.table[hash]
+            return true
+        }
+        return false
+    }
+
+    keyValues() {
+        return Object.values(this.table)
+    }
+
+    keys(){
+        return this.keyValues().map(valuePair => valuePair.key)
+
+    }
+
+    values(){
+        return this.keyValues().map(valuePair => valuePair.value)
+    }
+
+    size() {
+        return Object.keys(this.keyValues()).length;
+    }
+
+    isEmpty() {
+        return this.size() === 0
+    }
+
+    toString() {
+        if(this.isEmpty()){
+            return ''
+        }
+        const keys = Object.keys(this.table)
+        let objString = `{${keys[0]} => ${this.table[keys[0]].toString()}}`
+        for (let i = 1; i < keys.length; i++) {
+            objString = `${objString}, {${keys[i]} => ${this.table[keys[i]].toString()}}`
+        }
+        return objString
+    }
+
+}
+
+const hashTable = new HashTable();
+
+console.log("Adding Gandalf and his email")
+hashTable.put('Gandalf', 'gandalf@email.com')
+
+console.log("Adding Aragorn and his email")
+hashTable.put('Aragorn', 'aragorn@email.com')
+
+console.log("Adding Gimli and his email")
+hashTable.put('Gimli', 'gimli@email.com')
+
+console.log(hashTable.hashCode('Gandalf') + ' - Gandalf');
+console.log(hashTable.hashCode('Aragorn') + ' - Aragorn');
+console.log(hashTable.hashCode('Gimli') + ' - Gimli');
+console.log("Get Gandalf email: ", hashTable.get('Gandalf'));
+console.log("Get Aragorn email: ", hashTable.get('Aragorn'));
+console.log("Get Legolas email: ", hashTable.get('Legolas'));
+console.log("Remove Gandalf: ", hashTable.remove('Gandalf'))
+console.log("Get Gandalf email: ", hashTable.get('Gandalf'));
+
+console.log(hashTable.toString());
