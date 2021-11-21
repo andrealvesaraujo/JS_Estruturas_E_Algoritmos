343f068d0b10f1a71812fbee7a0097c14dc243e2
Andre Alves <andrealvesaraujo.1995@gmail.com>
2021-11-20 22:43:14 -0300

    Add MyHashTableSeparateChaining

--- Cap8/index.js
+++ Cap8/index.js
@@ -398,3 +398,75 @@ class LinkedList {
 
 }
 
+console.log("My HashTableSeparateChaining");
+
+class MyHashTableSeparateChaining {
+    constructor(toStrFn = defaultToString) {
+        this.toStrFn = toStrFn
+        this.table = {}
+    }
+
+    loseloseHashCode(key) {
+        if(typeof key === 'number') {       
+            return key
+        }
+        const tableKey = this.toStrFn(key)
+        let hash = 0
+        for(let i = 0; i < tableKey.length; i++) {
+            hash += tableKey.charCodeAt(i)
+        }
+        return hash % 37
+    }
+
+    hashCode(key) {
+        return this.loseloseHashCode(key)
+    }
+
+    put(key, value) {
+        if(key != null && value != null) {
+            const position = this.hashCode(key)
+            if(this.table[position] == null) {
+                this.table[position] = new LinkedList()
+            }
+            this.table[position].push(new ValuePair(key, value))
+            return true
+        }
+        return false
+    }
+
+    remove(key){
+        const position = this.hashCode(key)
+        const linkedList = this.table[position]
+        if(linkedList != null && !linkedList.isEmpty()) {
+            let current = linkedList.getHead()
+            while(current != null) {
+                if(current.element.key === key) {
+                    linkedList.remove(current.element)
+                    if(linkedList.isEmpty()) {
+                        delete this.table[position]
+                    }
+                    return true
+                }
+                current = current.element
+            }
+        }
+        return false
+    }
+
+    get(key){
+        const position = this.hashCode(key)
+        const linkedList = this.table[position]
+        if(linkedList != null && !linkedList.isEmpty()){
+            let current = linkedList.getHead()
+            while(current != null){
+                if(current.element.key === key) {
+                    return current.element.value
+                }
+            }
+            current = current.next
+        }
+        return undefined
+    }
+
+}
+
