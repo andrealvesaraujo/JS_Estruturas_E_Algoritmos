652aceb2b9e7d92df854dc1042b1380b18bca410
Andre Alves <andrealvesaraujo.1995@gmail.com>
2021-11-26 11:09:37 -0300

    Add Fibonnaci Interative and  Recursive and Memoization

--- Cap9/index.js
+++ Cap9/index.js
@@ -48,3 +48,40 @@ try {
 }
 
 console.log("----------------------------------------")
+
+console.log("Fibonnaci")
+
+function fibonnaciInteractive(n){
+    if(n<1) return 0
+    if(n<=2) return 1
+    let fibMinus2 = 0
+    let fibMinus1 = 1
+    let fibN = n
+    for(let i = 2; i <= n; i++){
+        fibN = fibMinus1 + fibMinus2
+        fibMinus2 = fibMinus1
+        fibMinus1 = fibN
+    }
+    return fibN
+}
+
+console.log("Fibonnaci Interactive of 5:", fibonnaciInteractive(5))
+
+function fibonnaci(n) {
+    if(n<1) return 0
+    if(n<=2) return 1
+    return fibonnaci(n-1) + fibonnaci(n-2)
+}
+
+console.log("Fibonnaci Recursive of 5:", fibonnaciInteractive(5))
+
+function fibonnaciMemoization(n) {
+    const memo = [0,1]
+    const fibonnaci = (n) =>{
+        if(memo[n] != null) return memo[n]
+        return memo[n] = fibonnaci(n-1, memo) + fibonnaci(n-2, memo)
+    }
+    return fibonnaci
+}
+
+console.log("Fibonnaci Memoization of 5:", fibonnaciMemoization()(5))
