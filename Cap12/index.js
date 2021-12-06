30a38965f5fe8ddbc2ee61c09822e521473c14c9
Andre Alves <andrealvesaraujo.1995@gmail.com>
2021-12-06 12:29:46 -0300

    Add Floyd-Warshall method to find shortest path

--- Cap12/index.js
+++ Cap12/index.js
@@ -316,7 +316,7 @@ graph = [[0,2,4,0,0,0],
 console.log("New Graph of adjacency matrix:")        
 console.log(graph)        
 
-const INF = Number.MAX_SAFE_INTEGER
+let INF = Number.MAX_SAFE_INTEGER
 const dijkstra = (graph, src) => {
     const dist = []
     const visited = []
@@ -349,5 +349,52 @@ const minDistance = (dist,visited) => {
     }
     return minIndex
 }
-console.log("Dijkstra algoritm to find shortest path:")
+console.log("Dijkstra algoritm to find shortest path from first Vertex:")
 console.log(dijkstra(graph, 0))
+
+console.log("---------------------------------------------------------")
+
+INF = Infinity;
+graph = [
+    [INF, 2, 4, INF, INF, INF],
+    [INF, INF, 2, 4, 2, INF],
+    [INF, INF, INF, INF, 3, INF],
+    [INF, INF, INF, INF, INF, 2],
+    [INF, INF, INF, 3, INF, 2],
+    [INF, INF, INF, INF, INF, INF]
+  ];
+
+console.log("New Graph of adjacency matrix:")        
+console.log(graph)    
+
+const floydWarshall = graph => {
+    const dist = []
+    const {length} = graph
+    for(let i=0; i< length; i++){
+        dist[i] = []
+        for(let j=0; j< length; j++){
+            if(i===j){
+                dist[i][j] = 0
+            } else if(!isFinite(graph[i][j])){
+                dist[i][j] = INF
+            } else {
+                dist[i][j] = graph[i][j]
+            }
+        }
+    }
+    for(let k = 0; k < length; k++){
+        for(let i=0; i< length; i++){
+            for(let j=0; j < length; j++){
+                if(dist[i][k] + dist[k][j] < dist[i][j]){
+                    dist[i][j] = dist[i][k] + dist[k][j]
+                }
+            }
+
+        }
+    }
+    return dist
+}
+
+console.log("FloydWarshall algoritm to find shortest path to all vertex:")
+
+console.log(floydWarshall(graph))
