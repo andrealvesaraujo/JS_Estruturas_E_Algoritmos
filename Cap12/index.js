d63597996487b2aacbd38e68eddc7ba555951a91
Andre Alves <andrealvesaraujo.1995@gmail.com>
2021-12-06 12:11:15 -0300

    Add dijkstra method to find shortest path

--- Cap12/index.js
+++ Cap12/index.js
@@ -98,7 +98,9 @@ const initializeColors = (vertices) => {
     return color;
 }
 
-console.log("------------Breadth First Search(BFS)------")
+console.log("---------------------------------------------------------")
+
+console.log("Breadth First Search(BFS)")
 
 
 export const breadthFirstSearch = (graph, startVertext, callback) => {
@@ -179,7 +181,9 @@ for(let i = 0; i < myVertices.length; i++){
     console.log(s)
 }
 
-console.log("------------Depth First Search(DFS)------")
+console.log("---------------------------------------------------------")
+console.log("Depth First Search(DFS)")
+
 
 const depthFirstSearch = (graph, callback) => {
     const vertices = graph.getVertices()
@@ -298,3 +302,52 @@ for (let count = 0; count < myVertices.length; count++){
 }
 
 console.log(s)  
+
+console.log("---------------------------------------------------------")
+
+
+graph = [[0,2,4,0,0,0],
+        [0,0,2,4,2,0],
+        [0,0,0,0,3,0],
+        [0,0,0,0,0,2],
+        [0,0,0,3,0,2],
+        [0,0,0,0,0,0]]
+
+console.log("New Graph of adjacency matrix:")        
+console.log(graph)        
+
+const INF = Number.MAX_SAFE_INTEGER
+const dijkstra = (graph, src) => {
+    const dist = []
+    const visited = []
+    const {length} = graph
+    for(let i = 0; i < length; i++) {
+        dist[i] = INF
+        visited[i] = false
+    }
+    dist[src] = 0
+    for(let i = 0; i < length - 1; i++){
+        const u = minDistance(dist, visited)
+        visited[u] = true
+        for(let v = 0; v < length; v++){
+            if(!visited[v] && graph[u][v] !==0 && dist[u] !== INF && dist[u]+graph[u][v] < dist[v]){   
+                dist[v] = dist[u] + graph[u][v]
+            }
+        }
+    }
+    return dist
+}
+
+const minDistance = (dist,visited) => {
+    let min = INF
+    let minIndex = -1
+    for(let v = 0; v < dist.length; v++){
+        if(visited[v] === false && dist[v] <=min){
+            min = dist[v]
+            minIndex = v
+        }
+    }
+    return minIndex
+}
+console.log("Dijkstra algoritm to find shortest path:")
+console.log(dijkstra(graph, 0))
