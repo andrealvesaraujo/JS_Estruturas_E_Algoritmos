42a5d66ac384122d61c63ee65bd5a9ac103bd9a7
Andre Alves <andrealvesaraujo.1995@gmail.com>
2021-12-07 16:24:09 -0300

    Add Merge and Quick Sort Algoritms

--- Cap13/index.js
+++ Cap13/index.js
@@ -114,3 +114,86 @@ console.log(array.join())
 console.log("")
 
 console.log("----------------------------------------------------------------")
+
+array = createNonSortedArray(8)
+console.log("Creating Non Sorted Array:")
+console.log(array.join())
+console.log("")
+
+function mergeSort(array, compareFn = defaultCompare) {
+    if(array.length > 1){
+        const {length} = array
+        const middle = Math.floor(length / 2)
+        const left = mergeSort(array.slice(0,middle), compareFn)
+        const right = mergeSort(array.slice(middle, length), compareFn)
+        array = merge(left, right, compareFn)
+    }
+    return array
+}
+
+function merge(left, right,compareFn) {
+    let i = 0
+    let j = 0 
+    const result = []
+    while(i<left.length && j<right.length) {
+        result.push(compareFn(left[i], right[j]) === Compare.LESS_THAN ? left[i++] : right[j++])
+    } 
+    return result.concat(i < left.length ? left.slice(i) : right.slice(j))
+}
+
+console.log("Merge Sort:")
+array =  mergeSort(array)
+console.log(array.join())
+console.log("")
+
+console.log("----------------------------------------------------------------")
+
+array = [3,5,1,6,4,7,2]
+console.log("Creating Non Sorted Array:")
+console.log(array.join())
+console.log("")
+
+function quickSort(array, compareFn = defaultCompare) {
+    return quick(array, 0, array.length - 1, compareFn)
+}
+
+function quick(array, left, right, compareFn) {
+    let index 
+    if(array.length > 1){
+        index = partition(array, left, right, compareFn)
+        if(left < index - 1){
+            quick(array, left, index-1, compareFn)
+        }
+        if(index < right){
+            quick(array, index, right, compareFn)
+        }
+    }
+    return array
+}   
+
+function partition(array, left, right,compareFn) {
+    const pivot = array[Math.floor((right+left)/2)];
+    let i = left
+    let j = right 
+    while(i<= j){
+        while(compareFn(array[i], pivot) === Compare.LESS_THAN){
+            i++
+        }
+        while(compareFn(array[j], pivot) === Compare.BIGGER_THAN){
+            j--
+        }
+        if(i<=j){
+            swap(array, i, j)
+            i++
+            j--
+        }
+    }
+    return i
+}
+
+console.log("Quick Sort:")
+array =  quickSort(array)
+console.log(array.join())
+console.log("")
+
+console.log("----------------------------------------------------------------")
