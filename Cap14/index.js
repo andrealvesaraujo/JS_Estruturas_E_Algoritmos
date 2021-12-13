2e01cf68a275a93989e99e0203b39b6425dafc4e
Andre Alves <andrealvesaraujo.1995@gmail.com>
2021-12-13 18:48:41 -0300

    Add Dynamic Programming solution of Matrix Chain Order problem

--- Cap14/index.js
+++ Cap14/index.js
@@ -62,7 +62,7 @@ export function binarySearch(array, value, compareFn = defaultCompare){
     const sortedArray = quickSort(array)
     const low = 0 
     const high = array.length - 1
-    return binarySearchRecursive(array,value,low,high,compareFn)
+    return binarySearchRecursive(sortedArray,value,low,high,compareFn)
 }
 
 export function binarySearchRecursive(array,value,low,high,compareFn = defaultCompare){
@@ -178,7 +178,6 @@ console.log("[{2,3},{3,4},{4,5}]")
 console.log("Total value that can be carried : ", knapSack(capacity,weights, values,n))
 console.log("")
 
-
 console.log("----------------------------------------------------------------")
 
 console.log("Longest Common Subsequence(LCS)")
@@ -236,4 +235,53 @@ function printSolution(solution, wordX,m,n ) {
 console.log("word1: 'acbaed'")
 console.log("word2: 'abcadf'")
 lcs("acbaed", "abcadf")
+console.log("")
+
+console.log("----------------------------------------------------------------")
+
+console.log("Matrix Chain Order")
+
+function matrixChainOrder(p) {
+    const n = p.length
+    const m = []
+    const s= []
+    for (let i=0; i<=n; i++) {
+        s[i] = []
+        for (let j = 0; j <=n ; j++){
+            s[i][j] = 0
+        }
+    }
+    for (let i=0; i<=n; i++) {
+        m[i] = []
+        m[i][i]  = 0
+    }
+    for(let l = 2; l< n; l++ ){
+        for(let i = 1; i<= (n-l) + 1; i++) {
+            const j = (i+l) - 1
+            m[i][j] = Number.MAX_SAFE_INTEGER
+            for(let k = i; k <= j-1; k++){
+                const q = m[i][k] + m[k+1][j] + ((p[i-1] * p[k]) * p[j])
+                if(q<m[i][j]){
+                    m[i][j] = q
+                    s[i][j] = k
+                } 
+            }
+        }
+    }
+    printOptimalParenthesis(s, 1, n-1)
+}
+
+function printOptimalParenthesis(s,i,j) {
+    if(i===j){
+        console.log("A[" + i + "]")
+    } else {
+        console.log("(")
+        printOptimalParenthesis(s,i, s[i][j])
+        printOptimalParenthesis(s,s[i][j] + 1, j)
+        console.log(")")
+    }
+}
+
+const p = [10,100,5,50,1]
+matrixChainOrder(p)
 console.log("")
