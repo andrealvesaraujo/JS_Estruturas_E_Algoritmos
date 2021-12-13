ef2e642d735e17e966869c7baa0fa5c59cef013d
Andre Alves <andrealvesaraujo.1995@gmail.com>
2021-12-13 16:31:56 -0300

    Add Dynamic Programming first solution to the problem of MinCoinChange

--- Cap14/index.js
+++ Cap14/index.js
@@ -85,3 +85,44 @@ let resultSearch  =  binarySearch(array,2)
 console.log("Search number 2. Index of Array: ",resultSearch)
 console.log("")
 
+console.log("----------------------------------------------------------------")
+
+console.log("Dynamic Programming")
+
+console.log("----------------------------------------------------------------")
+
+console.log("Min Coin Change Problem")
+
+function minCoinChange(coins, amount) {
+    const cache = []
+    const makeChange = (value) => {
+        if(!value){
+            return []
+        }
+        if(cache[value]){
+            return cache[value]
+        }
+        let min = []
+        let newMin
+        let newAmount
+        for(let i = 0; i < coins.length; i++){
+            const coin = coins[i]
+            newAmount = value - coin
+            if(newAmount>=0){
+                newMin = makeChange(newAmount)
+            }
+            if(newAmount>=0 && (newMin.length < min.length -1 || !min.length) && (newMin.length || !newAmount)){
+                min = [coin].concat(newMin)
+                // console.log("new Min " + min + ' for ' + amount)
+            }
+        }
+        return (cache[value]=min)
+    }
+    return makeChange(amount)
+}
+
+let resultMinCoin  =  minCoinChange([1,5,10,25], 36)
+console.log("minCoinChange of 36 in [1,5,10,25]: ", resultMinCoin)
+resultMinCoin  =  minCoinChange([1,3,4], 6)
+console.log("minCoinChange of 6 in [1,3,4]: ", resultMinCoin)
+console.log("")
