44ab2f8044d3c8cb9477b5fa6a81003628d5352c
Andre Alves <andrealvesaraujo.1995@gmail.com>
2021-12-13 18:14:11 -0300

    Add Dynamic Programming solution of  KnapSack problem

--- Cap14/index.js
+++ Cap14/index.js
@@ -87,7 +87,7 @@ console.log("")
 
 console.log("----------------------------------------------------------------")
 
-console.log("Dynamic Programming")
+console.log("Dynamic Programming(DP)")
 
 console.log("----------------------------------------------------------------")
 
@@ -125,4 +125,56 @@ let resultMinCoin  =  minCoinChange([1,5,10,25], 36)
 console.log("minCoinChange of 36 in [1,5,10,25]: ", resultMinCoin)
 resultMinCoin  =  minCoinChange([1,3,4], 6)
 console.log("minCoinChange of 6 in [1,3,4]: ", resultMinCoin)
+console.log("")
+
+
+console.log("----------------------------------------------------------------")
+
+console.log("KnapSack Problem")
+
+function knapSack(capacity, weights, values, n) {
+    
+    const kS = []
+    for(let i = 0; i <= n; i++) {
+        kS[i] = []
+    }
+    for(let i = 0; i <= n; i++) {
+        for(let w = 0; w <= capacity; w++) {
+            if(i===0 || w===0) {
+                kS[i][w] = 0
+            }else if(weights[i-1] <=w){
+                const a = values[i-1] + kS[i-1][w-weights[i-1]]
+                const b = kS[i-1][w]
+                kS[i][w] = a > b ? a : b
+            } else{
+                kS[i][w] = kS[i-1][w]
+            }
+        }
+    }
+    findValues(n, capacity,kS, weights, values,)
+    return kS[n][capacity]
+}
+
+function findValues(n, capacity, kS, weights, values) {
+    let i = n
+    let k = capacity
+    console.log("Items that are part of the solution:")
+    while(i> 0 && k>0){
+        if(kS[i][k] !== kS[i-1][k]){
+            console.log(`item ${i} can be part of solution w,v: ${weights[i-1]}, ${values[i-1]}`)
+            i--
+            k-= kS[i][k]
+        }else{
+            i--
+        }
+    }
+}
+
+const weights = [2,3,4]
+const values  = [3,4,5] 
+const capacity = 5
+const n = values.length
+console.log("Itens {weight,value}:")
+console.log("[{2,3},{3,4},{4,5}]")
+console.log("Total value that can be carried : ", knapSack(capacity,weights, values,n))
 console.log("")
