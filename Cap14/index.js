b6e87daed06485ee0b9c4460bbab20dbebaa1c11
Andre Alves <andrealvesaraujo.1995@gmail.com>
2021-12-13 18:28:06 -0300

    Add Dynamic Programming solution of  Longest Common Subsequence problem

--- Cap14/index.js
+++ Cap14/index.js
@@ -127,7 +127,6 @@ resultMinCoin  =  minCoinChange([1,3,4], 6)
 console.log("minCoinChange of 6 in [1,3,4]: ", resultMinCoin)
 console.log("")
 
-
 console.log("----------------------------------------------------------------")
 
 console.log("KnapSack Problem")
@@ -177,4 +176,64 @@ const n = values.length
 console.log("Itens {weight,value}:")
 console.log("[{2,3},{3,4},{4,5}]")
 console.log("Total value that can be carried : ", knapSack(capacity,weights, values,n))
+console.log("")
+
+
+console.log("----------------------------------------------------------------")
+
+console.log("Longest Common Subsequence(LCS)")
+
+function lcs(wordX, wordY) {
+    const m = wordX.length
+    const n = wordY.length
+    const l = []
+    const solution = []
+    for (let i = 0; i <= m; i++) {
+        l[i] = []
+        solution[i] = []
+        for (let j = 0; j <=n ; j++) {
+            l[i][j] = 0
+            solution[i][j] = '0'
+        }
+    }
+    for (let i = 0; i <= m; i++) {
+        for (let j = 0; j <= n; j++) {
+            if(i===0 || j===0){
+                l[i][j] = 0
+            }else if(wordX[i-1] === wordY[j-1]){
+                l[i][j] = l[i-1][j-1] + 1
+                solution[i][j] = 'diagonal'
+            }else{
+                const a = l[i-1][j]
+                const b = l[i][j-1]
+                l[i][j] = a > b ? a : b
+                solution[i][j] = (l[i][j] == l[i-1][j]) ? 'top' : 'left'
+            }
+        }
+    }
+    printSolution(solution, wordX, m,n)
+}
+
+function printSolution(solution, wordX,m,n ) {
+    let a = m
+    let b = n
+    let x = solution[a][b]
+    let answer = ''
+    while(x !== '0'){
+        if(solution[a][b] === 'diagonal'){
+            answer = wordX[a-1] + answer
+            a--
+            b--
+        } else if(solution[a][b] === 'left'){
+            b--
+        } else if(solution[a][b] === 'top'){
+            a--
+        }
+        x=solution[a][b]
+    }
+    console.log('lsc: '+ answer)
+}
+console.log("word1: 'acbaed'")
+console.log("word2: 'abcadf'")
+lcs("acbaed", "abcadf")
 console.log("")
