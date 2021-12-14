276735f15c95aafdf1079569c0d3df101f1cf142
Andre Alves <andrealvesaraujo.1995@gmail.com>
2021-12-14 12:26:03 -0300

    Add Backtracking Algorithm solution of  rat In A Maze problem

--- Cap14/index.js
+++ Cap14/index.js
@@ -288,7 +288,7 @@ console.log("")
 
 console.log("----------------------------------------------------------------")
 
-console.log("Greedy Algorithm")
+console.log("Greedy Algorithms")
 
 console.log("----------------------------------------------------------------")
 
@@ -341,3 +341,68 @@ console.log("Itens {weight,value} of Capacity 6:")
 console.log("[{2,3},{3,4},{4,5}]")
 console.log("Total value that can be carried : ", knapSackGreed(capacity,weights, values))
 console.log("")
+
+console.log("----------------------------------------------------------------")
+
+console.log("Backtracking Algorithms")
+
+console.log("----------------------------------------------------------------")
+
+console.log("Rat in a Maze")
+
+export function ratInAMaze(maze) {
+    const solution = []
+    for(let i = 0; i < maze.length; i++) {
+        solution[i] = []
+        for(let j = 0; j < maze[i].length ; j++){
+            solution[i][j] = 0
+        }
+    }
+    if(findPath(maze,0,0,solution) === true){
+        return solution
+    }
+    return "NO PATH FOUND"
+}
+
+function findPath(maze, x, y, solution){
+    const n = maze.length
+    if(x === n-1 && y === n-1){
+        solution[x][y] = 1
+        return true
+    }
+    if(isSafe(maze,x,y) === true){
+        solution[x][y] = 1
+        if(findPath(maze,x+1,y, solution)){
+            return true
+        }
+        if(findPath(maze,x,y+1, solution)){
+            return true
+        }
+        solution[x][y] = 0
+        return false
+    }
+    return false
+}
+
+function isSafe(maze,x,y){
+    const n = maze.length
+    if(x>=0 && y>=0 && x<n && y<n && maze[x][y] !==0){
+        return true
+    }
+    return false
+}
+
+const maze = [
+    [1,0,0,0],
+    [1,1,1,1],
+    [0,0,1,0],
+    [0,1,1,1],
+
+]
+
+let resultBacktracking =  ratInAMaze(maze)
+console.log("Rat in a Maze Path: ")
+console.log(resultBacktracking)
+console.log("")
+
+console.log("----------------------------------------------------------------")
